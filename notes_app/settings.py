f67af40bf86ec55a11a3faa1adbb06b3d460dbb4
Ben Turner <turner485@hotmail.co.uk>
2023-11-23 15:54:48 +0000

    run build for dep

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'http://13.41.75.26:8000/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
